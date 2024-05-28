47c227fb95d16f2a1f58abb10b37e4f2f25f3205
zhangyushao <ling0017@outlook.com>
2024-05-28 15:29:18 +0800

    first commit

---
+++ native/hub/src/lib.rs
@@ -0,0 +1,22 @@
+//! This `hub` crate is the
+//! entry point of the Rust logic.
+
+mod messages;
+
+use tokio;
+// use tokio_with_wasm::tokio; // Uncomment this line to target the web
+
+rinf::write_interface!();
+
+// Use `tokio::spawn` to run concurrent tasks.
+// Always use non-blocking async functions
+// such as `tokio::fs::File::open`.
+// If you really need to use blocking code,
+// use `tokio::task::spawn_blocking`.
+async fn main() {
+    use messages::basic::*;
+    // Send signals to Dart like below.
+    SmallNumber { number: 7 }.send_signal_to_dart();
+    // Get receivers that listen to Dart signals like below.
+    let _ = SmallText::get_dart_signal_receiver();
+}
